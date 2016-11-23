43c052beb84ebb7691a1f9423b976503463aae8f
David Irvine <aviddiviner@gmail.com>
2016-11-23 14:07:28 -0500

    Move macOS specific code to its own file, with build flags

--- main.go
+++ main.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"strings"
 )
 
 func main() {
-	top := NewTop(1)
+	top := NewTop(5)
+	log.Println("Waiting...")
 	for {
 		select {
 		case <-top.NextTick:
 			for _, p := range top.ProcessList() {
 				if strings.HasPrefix(p.Command, "Spotify") {
 					if p.Command == "Spotify" {
-						fmt.Printf("** %#v\n", p)
+						log.Printf("** %#v\n", p)
 					} else {
-						fmt.Printf("%#v\n", p)
+						log.Printf("%#v\n", p)
 					}
 				}
 			}
+			log.Println("---")
 		}
 	}
 }
