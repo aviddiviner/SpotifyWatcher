f2553806838a25a7d2961aa93d46234ec62b50fc
David Irvine <aviddiviner@gmail.com>
2016-12-07 10:00:51 -0500

    Add median function

--- math.go
+++ math.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"math"
+	"sort"
+)
+
 // Not safe for concurrent use.
 
 type SlidingWindow struct {
@@ -8,6 +13,8 @@ type SlidingWindow struct {
 	window []interface{}
 }
 
+// Append adds an element to the end of the sliding window. If the window size
+// has been reached, the oldest element will be overwritten.
 func (s *SlidingWindow) Append(f interface{}) {
 	if len(s.window) < s.size { // Grow the window.
 		s.window = s.window[:s.idx+1]
@@ -17,17 +24,28 @@ func (s *SlidingWindow) Append(f interface{}) {
 	return
 }
 
+// Reset clears the window, as if brand new.
 func (s *SlidingWindow) Reset() {
 	s.idx = 0
 	s.window = s.window[:0]
 }
 
+// Len implements sort.Interface.
 func (s *SlidingWindow) Len() int {
 	return len(s.window)
 }
 
+// Swap implements sort.Interface.
 func (s *SlidingWindow) Swap(i, j int) {
 	s.window[i], s.window[j] = s.window[j], s.window[i]
+}
+
+// Copy returns an identical copy of the sliding window.
+func (s *SlidingWindow) Copy() *SlidingWindow {
+	slice := make([]interface{}, s.size)
+	slice = slice[:len(s.window)]
+	copy(slice, s.window)
+	return &SlidingWindow{size: s.size, idx: s.idx, window: slice}
 }
 
 func NewSlidingWindow(size int) *SlidingWindow {
@@ -44,39 +62,34 @@ type MovingAvg struct {
 	*SlidingWindow
 }
 
-func (v *MovingAvg) Less(i, j int) bool {
-	var a, b float64
-	switch n := v.window[i].(type) {
+// asFloat casts window[idx] from interface{} to float64
+func (v *MovingAvg) asFloat(idx int) (f float64) {
+	switch n := v.window[idx].(type) {
 	case float64:
-		a = n
+		f = n
 	case int:
-		a = float64(n)
+		f = float64(n)
 	default:
+		f = math.NaN()
 	}
-	switch n := v.window[j].(type) {
-	case float64:
-		a = n
-	case int:
-		a = float64(n)
-	default:
-	}
-	return a < b
+	return f
 }
 
+// Less implements sort.Interface.
+func (v *MovingAvg) Less(i, j int) bool {
+	return v.asFloat(i) < v.asFloat(j)
+}
+
+// SumFn applies some function fn to each element, and returns the sum.
 func (v *MovingAvg) SumFn(fn func(float64) float64) float64 {
 	total := 0.0
-	for _, n := range v.window {
-		switch f := n.(type) {
-		case float64:
-			total += fn(f)
-		case int:
-			total += fn(float64(f))
-		default:
-		}
+	for i := range v.window {
+		total += fn(v.asFloat(i))
 	}
 	return total
 }
 
+// Average returns the mean value.
 func (v *MovingAvg) Average() float64 {
 	total := v.SumFn(func(f float64) float64 { return f })
 	return total / float64(v.Len())
@@ -84,4 +97,25 @@ func (v *MovingAvg) Average() float64 {
 
 func NewMovingAvg(size int) *MovingAvg {
 	return &MovingAvg{NewSlidingWindow(size)}
+}
+
+// Median returns the middle value.
+func (v *MovingAvg) Median() float64 {
+	if v.Len() < 1 {
+		return math.NaN()
+	}
+	if v.Len() < 2 {
+		return v.asFloat(0) // (v.idx + v.size - 1) % v.size
+	}
+	vc := &MovingAvg{v.Copy()}
+	sort.Sort(vc)
+	// TODO: func Quantile(p float64). Instead of 0.5, we can use p.
+	// Also, special cases:
+	//  if p <= 0 return first value
+	//  if p >= 1 return last value
+	h := float64(vc.Len()-1) * 0.5
+	i := int(math.Floor(h))
+	a := vc.asFloat(i)
+	b := vc.asFloat(i + 1)
+	return a + (b-a)*(h-float64(i))
 }
--- math_test.go
+++ math_test.go
@@ -94,3 +94,48 @@ func TestMovingAvgSumFn(t *testing.T) {
 	avg.Reset()
 	assertFloatsEqual(t, avg.SumFn(greaterThan1), 0)
 }
+
+func TestMovingAvgMedian(t *testing.T) {
+	avg := NewMovingAvg(5)
+
+	// Single value
+	avg.Append(0.8)
+	assertFloatsEqual(t, avg.Median(), 0.8)
+
+	// Middle of 3 values
+	avg.Append(1.8)
+	avg.Append(2)
+	assertFloatsEqual(t, avg.Median(), 1.8)
+
+	// Test reset and try again
+	avg.Reset()
+	avg.Append(0.8)
+	assertFloatsEqual(t, avg.Median(), 0.8)
+	avg.Append(1.8)
+	avg.Append(2)
+	assertFloatsEqual(t, avg.Median(), 1.8)
+
+	// Test interpolating between even number of values
+	avg.Reset()
+	avg.Append(1)
+	avg.Append(2)
+	assertFloatsEqual(t, avg.Median(), 1.5)
+	avg.Append(3)
+	avg.Append(3)
+	assertFloatsEqual(t, avg.Median(), 2.5)
+
+	// Test average still works
+	assertFloatsEqual(t, avg.Average(), 9.0/4)
+
+	// Test sorting for median doesn't break the sliding window
+	avg.Append(5)
+	avg.Append(1)
+	avg.Append(3)
+	avg.Append(9)
+	avg.Append(7)
+	assertFloatsEqual(t, avg.Median(), 5)
+	assertFloatsEqual(t, avg.Average(), 5)
+	avg.Append(0)
+	assertFloatsEqual(t, avg.Median(), 3)
+	assertFloatsEqual(t, avg.Average(), 4)
+}
